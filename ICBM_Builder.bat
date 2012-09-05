4ab9d9ddd0c1fbad9eda1f8564149b05508f844a
Calclavia <henry@calclavia.com>
2012-09-05 18:43:41 +0800

    Improved Build Script

--- ICBM_Builder.bat
+++ ICBM_Builder.bat
@@ -2,27 +2,36 @@
 set /p MODVERSION=<modversion.txt
 set /p CurrentBuild=<buildnumber.txt
 set /a BUILD_NUMBER=%CurrentBuild%+1
-del buildnumber.txt
-echo %BUILD_NUMBER% >> buildnumber.txt
+echo %BUILD_NUMBER%>buildnumber.txt
 
+::BUILD
 runtime\bin\python\python_mcp runtime\recompile.py %*
 runtime\bin\python\python_mcp runtime\reobfuscate.py %*
 
-cd reobf\minecraft\
-"C:\Users\Henry\Documents\GitHub\ICBM\7za.exe" a "E:\Document\Computer Science\Minecraft Modding\Releases\ICBM Builds\ICBM_v%MODVERSION%.%BUILD_NUMBER%.zip" "atomicscience\"
+set FILE_NAME=ICBM_v%MODVERSION%.%BUILD_NUMBER%.zip
 
-"C:\Users\Henry\Documents\GitHub\ICBM\7za.exe" a "E:\Document\Computer Science\Minecraft Modding\Releases\ICBM Builds\ICBM_v%MODVERSION%.%BUILD_NUMBER%.zip" "icbm\"
+::ZIP-UP
+cd reobf\minecraft\
+"C:\Users\Henry\Documents\GitHub\ICBM\7za.exe" a "E:\Document\Computer Science\Minecraft Modding\Releases\ICBM Builds\%FILE_NAME%" "atomicscience\"
+
+"C:\Users\Henry\Documents\GitHub\ICBM\7za.exe" a "E:\Document\Computer Science\Minecraft Modding\Releases\ICBM Builds\%FILE_NAME%" "icbm\"
 
 cd ..\..\
-"C:\Users\Henry\Documents\GitHub\ICBM\7za.exe" a "E:\Document\Computer Science\Minecraft Modding\Releases\ICBM Builds\ICBM_v%MODVERSION%.%BUILD_NUMBER%.zip" "mcmod*.info"
+"C:\Users\Henry\Documents\GitHub\ICBM\7za.exe" a "E:\Document\Computer Science\Minecraft Modding\Releases\ICBM Builds\%FILE_NAME%" "mcmod*.info"
 
 cd includes\
-"C:\Users\Henry\Documents\GitHub\ICBM\7za.exe" a "E:\Document\Computer Science\Minecraft Modding\Releases\ICBM Builds\ICBM_v%MODVERSION%.%BUILD_NUMBER%.zip" "icbm\"
+"C:\Users\Henry\Documents\GitHub\ICBM\7za.exe" a "E:\Document\Computer Science\Minecraft Modding\Releases\ICBM Builds\%FILE_NAME%" "icbm\"
 
 "C:\Users\Henry\Documents\GitHub\ICBM\7za.exe" a "E:\Document\Computer Science\Minecraft Modding\Backup\icbm\ICBM_v%MODVERSION%.%BUILD_NUMBER%_backup.zip" "icbm\"
 cd ..\
 
 cd src\
 "C:\Users\Henry\Documents\GitHub\ICBM\7za.exe" a "E:\Document\Computer Science\Minecraft Modding\Backup\icbm\ICBM_v%MODVERSION%.%BUILD_NUMBER%_backup.zip" "*\icbm\"
+
+::COPY TO DROPBOX
+xcopy "E:\Document\Computer Science\Minecraft Modding\Releases\ICBM Builds\%FILE_NAME%" "C:\Users\Henry\Dropbox\ICBM" /E
+
+echo %FILE_NAME%>>"C:\Users\Henry\Dropbox\ICBM\info.txt"
+echo Done building %FILE_NAME% for UE %UE_VERSION%
 
 pause
