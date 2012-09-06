ea5d2a33eeb15c16c74b4f8614e31847156cef7e
Calclavia <henry@calclavia.com>
2012-09-06 18:51:38 +0800

    Builder Update

--- ICBM_Builder.bat
+++ ICBM_Builder.bat
@@ -2,13 +2,15 @@
 set /p MODVERSION=<modversion.txt
 set /p CurrentBuild=<buildnumber.txt
 set /a BUILD_NUMBER=%CurrentBuild%+1
-echo %BUILD_NUMBER%>buildnumber.txt
+echo %BUILD_NUMBER% >buildnumber.txt
+
+set FILE_NAME=ICBM_v%MODVERSION%.%BUILD_NUMBER%.zip
+
+echo Starting to build %FILE_NAME%
 
 ::BUILD
 runtime\bin\python\python_mcp runtime\recompile.py %*
 runtime\bin\python\python_mcp runtime\reobfuscate.py %*
-
-set FILE_NAME=ICBM_v%MODVERSION%.%BUILD_NUMBER%.zip
 
 ::ZIP-UP
 cd reobf\minecraft\
@@ -26,12 +28,14 @@ cd includes\
 cd ..\
 
 cd src\
-"C:\Users\Henry\Documents\GitHub\ICBM\7za.exe" a "E:\Document\Computer Science\Minecraft Modding\Backup\icbm\ICBM_v%MODVERSION%.%BUILD_NUMBER%_backup.zip" "*\icbm\"
+"C:\Users\Henry\Documents\GitHub\ICBM\7za.exe" a "E:\Document\Computer Science\Minecraft Modding\Backup\icbm\ICBM_v%MODVERSION%.%BUILD_NUMBER%_backup.zip" "*\icbm\" -pHENRY
 
 ::COPY TO DROPBOX
 xcopy "E:\Document\Computer Science\Minecraft Modding\Releases\ICBM Builds\%FILE_NAME%" "C:\Users\Henry\Dropbox\ICBM" /E
 
-echo %FILE_NAME%>>"C:\Users\Henry\Dropbox\ICBM\info.txt"
+echo What version is Universal Electricity for this build?
+set /p UE_VERSION=
+echo %FILE_NAME% %UE_VERSION%>>"C:\Users\Henry\Dropbox\ICBM\info.txt"
 echo Done building %FILE_NAME% for UE %UE_VERSION%
 
 pause
