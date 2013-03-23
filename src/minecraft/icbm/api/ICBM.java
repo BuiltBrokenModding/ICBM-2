6e74848f915bd442c90efca54b939a424caa81cc
Henry Mao <admin@calclavia.com>
2013-03-23 10:38:40 +0800

    Version Bump to 1.1.1

--- src/minecraft/icbm/api/ICBM.java
+++ src/minecraft/icbm/api/ICBM.java
@@ -26,7 +26,7 @@ public class ICBM
 	/**
 	 * The version of ICBM.
 	 */
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.1";
 
 	/**
 	 * Configuration file for ICBM.
