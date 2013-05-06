6760d3486cd74c9ed796fd41a387b52489c0cd5f
Robert Seifert <rseifert.phone@gmail.com>
2013-05-06 02:29:41 -0400

    updated subs

--- src/icbm/gangshao/turret/sentries/TileEntityGunTurret.java
+++ src/icbm/gangshao/turret/sentries/TileEntityGunTurret.java
@@ -55,7 +55,7 @@ public class TileEntityGunTurret extends TileEntityAutoTurret
 		if (this.onFire())
 		{
 			this.worldObj.playSoundEffect(this.xCoord, this.yCoord, this.zCoord, "icbm.machinegun", 5F, 1F);
-			this.sendShotToClient(new Vector3(this.target));
+			this.sendShotToClient(this.getMuzzle());
 		}
 	}
 
