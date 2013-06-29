7367eb9a13894f121a9588b6a46c248673655eb0
Calclavia <admin@calclavia.com>
2013-06-29 12:24:39 -0400

    Updated MFFS API

--- src/mffs/api/IProjector.java
+++ src/mffs/api/IProjector.java
@@ -1,15 +1,6 @@
 package mffs.api;
 
-import java.util.Set;
-
-import mffs.api.modules.IModule;
-import mffs.api.modules.IModuleAcceptor;
-import mffs.api.modules.IProjectorMode;
 import net.minecraft.inventory.IInventory;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.common.ForgeDirection;
-import universalelectricity.core.vector.Vector3;
-import universalelectricity.prefab.implement.IRotatable;
 
 /**
  * Also extends IDisableable, IFortronFrequency
@@ -17,34 +8,8 @@ import universalelectricity.prefab.implement.IRotatable;
  * @author Calclavia
  * 
  */
-public abstract interface IProjector extends IInventory, IModuleAcceptor, IRotatable, IBiometricIdentifierLink
+public abstract interface IProjector extends IInventory, IBiometricIdentifierLink, IFieldInteraction
 {
-
-	/**
-	 * @return Is the projector active?
-	 */
-	public boolean isActive();
-
-	/**
-	 * Gets the mode of the projector, mainly the shape and size of it.
-	 */
-	public IProjectorMode getMode();
-
-	public ItemStack getModeStack();
-
-	/**
-	 * Transformation information functions. Returns CACHED information unless the cache is cleared.
-	 */
-	public Vector3 getTranslation();
-
-	public Vector3 getPositiveScale();
-
-	public Vector3 getNegativeScale();
-
-	public int getRotationYaw();
-
-	public int getRotationPitch();
-
 	/**
 	 * Projects a force field.
 	 */
@@ -56,38 +21,9 @@ public abstract interface IProjector extends IInventory, IModuleAcceptor, IRotat
 	public void destroyField();
 
 	/**
-	 * Gets the unspecified, direction-unspecific module slots on the left side of the GUI.
-	 */
-	public int[] getModuleSlots();
-
-	/**
-	 * @param module - The module instance.
-	 * @param direction - The direction facing.
-	 * @return Gets the amount of modules based on the side.
-	 */
-	public int getSidedModuleCount(IModule module, ForgeDirection... direction);
-
-	/**
-	 * Gets the slot IDs based on the direction given.
-	 */
-	public int[] getSlotsBasedOnDirection(ForgeDirection direction);
-
-	/**
-	 * * @return Gets all the blocks that are occupying the force field.
-	 */
-	public Set<Vector3> getCalculatedField();
-
-	/**
 	 * @return The speed in which a force field is constructed.
 	 */
 	public int getProjectionSpeed();
-
-	/**
-	 * Gets the interior points of the projector. This might cause lag so call sparingly.
-	 * 
-	 * @return
-	 */
-	public Set<Vector3> getInteriorPoints();
 
 	/**
 	 * * @return The amount of ticks this projector has existed in the world.
--- src/mffs/api/security/Permission.java
+++ src/mffs/api/security/Permission.java
@@ -2,24 +2,25 @@ package mffs.api.security;
 
 public class Permission
 {
+	// TODO: FIX NAMES
 	/**
 	 * Force Field Wrap - Allows a player to go through force fields.
 	 */
 	public static final Permission FORCE_FIELD_WARP = new Permission(0, "warp");
 	/**
-	 * Access - Allows to open GUIs and activate blocks.
+	 * Place Access - Allows to open GUIs and activate blocks.
 	 */
 	public static final Permission BLOCK_ALTER = new Permission(1, "blockPlaceAccess");
 	/**
-	 * Configure - Allows to configure security stations.
+	 * Block Access - Allows block access and opening GUIs.
 	 */
 	public static final Permission BLOCK_ACCESS = new Permission(2, "blockAccess");
 	/**
-	 * Stay - Allows to stay in defense station zone.
+	 * Configure - Allows to configure biometric identifiers.
 	 */
 	public static final Permission SECURITY_CENTER_CONFIGURE = new Permission(3, "configure");
 	/**
-	 * Bypass Confiscation - Allows the bypassing of defense station confiscation.
+	 * Bypass Confiscation - Allows the bypassing of interdiction matrix confiscation.
 	 */
 	public static final Permission BYPASS_INTERDICTION_MATRIX = new Permission(4, "bypassDefense");
 	/**
@@ -27,7 +28,7 @@ public class Permission
 	 */
 	public static final Permission DEFENSE_STATION_CONFISCATION = new Permission(5, "bypassConfiscation");
 	/**
-	 * Warp - Allows going through force fields.
+	 * Remote Control - Allows player to remotely control blocks with the remote.
 	 */
 	public static final Permission REMOTE_CONTROL = new Permission(6, "remoteControl");
 
