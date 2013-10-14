59add6682e90f6a2065f1ca0d46aedcedc1d1173
DarkGuardsman <rseifert.phone@gmail.com>
2013-10-13 20:18:23 -0400

    Change IAATarget
    Depricated doDamage and added a better method in its place. Both should
    be called until 1.7 MC update.

--- src/sentry/icbm/api/sentry/IAATarget.java
+++ src/sentry/icbm/api/sentry/IAATarget.java
@@ -1,7 +1,9 @@
 package icbm.api.sentry;
 
+import net.minecraft.util.DamageSource;
+
 /** Apply this to an entity if it is meant to be targeted by the AA Turret.
- * 
+ *
  * @author Calclavia */
 public interface IAATarget
 {
@@ -10,15 +12,23 @@ public interface IAATarget
     public void destroyCraft();
 
     /** Applies damage to the the target
-     * 
+     *
      * @param damage - damage in half HP
      * @return the amount of HP left. Return -1 if this target can't take damage, and will be chance
      * killed. Return 0 if this target is dead and destroyCraft() will be called. */
+    @Deprecated
     public int doDamage(int damage);
+
+    /** Attacks the aa target in the same way as a normal EntityLiving
+     *
+     * @param source - Damage source
+     * @param damage - actual damage to apply
+     * @return true if the damage was applied */
+    public boolean attackEntityFrom(DamageSource source, float damage);
 
     /** Can this be targeted by automated targeting systems or AIs. Used to implement radar jammers,
      * cloaking devices, and other addons for the Entity being targeted
-     * 
+     *
      * @param entity - entity that is targeting this, can be an Entity, EntityLiving, or TileEntity
      * @return true if it can */
     public boolean canBeTargeted(Object entity);
