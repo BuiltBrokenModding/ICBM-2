2bc2abd6c83418962c7a78910ad9424291f10cdd
Calclavia <admin@calclavia.com>
2014-01-18 19:24:11 +0800

    Fixed cruise launcher GUI and packets

--- src/main/java/icbm/explosion/machines/TileLauncherPrefab.java
+++ src/main/java/icbm/explosion/machines/TileLauncherPrefab.java
@@ -13,155 +13,155 @@ import dan200.computer.api.IPeripheral;
 
 public abstract class TileLauncherPrefab extends TileFrequency implements ILauncherController, IPeripheral, IRedstoneReceptor
 {
-    @Override
-    public void initiate()
-    {
-        super.initiate();
-        MissileLauncherRegistry.registerLauncher(this);
-    }
+	@Override
+	public void initiate()
+	{
+		super.initiate();
+		MissileLauncherRegistry.registerLauncher(this);
+	}
 
-    @Override
-    public void invalidate()
-    {
-        MissileLauncherRegistry.unregisterLauncher(this);
-        super.invalidate();
-    }
+	@Override
+	public void invalidate()
+	{
+		MissileLauncherRegistry.unregisterLauncher(this);
+		super.invalidate();
+	}
 
-    protected Vector3 targetPos = null;
+	protected Vector3 targetPos = null;
 
-    @Override
-    public boolean canConnect(ForgeDirection direction)
-    {
-        return true;
-    }
+	@Override
+	public boolean canConnect(ForgeDirection direction)
+	{
+		return true;
+	}
 
-    @Override
-    public Vector3 getTarget()
-    {
-        if (this.targetPos == null)
-        {
-            if (this.getLauncherType() == LauncherType.CRUISE)
-            {
-                this.targetPos = new Vector3(this.xCoord, this.yCoord, this.zCoord);
-            }
-            else
-            {
-                this.targetPos = new Vector3(this.xCoord, 0, this.zCoord);
-            }
-        }
+	@Override
+	public Vector3 getTarget()
+	{
+		if (this.targetPos == null)
+		{
+			if (this.getLauncherType() == LauncherType.CRUISE)
+			{
+				this.targetPos = new Vector3(this.xCoord, this.yCoord, this.zCoord);
+			}
+			else
+			{
+				this.targetPos = new Vector3(this.xCoord, 0, this.zCoord);
+			}
+		}
 
-        return this.targetPos;
-    }
+		return this.targetPos;
+	}
 
-    @Override
-    public void setTarget(Vector3 target)
-    {
-        this.targetPos = target.floor();
-    }
+	@Override
+	public void setTarget(Vector3 target)
+	{
+		this.targetPos = target.floor();
+	}
 
-    @Override
-    public String getType()
-    {
-        return "ICBMLauncher";
-    }
+	@Override
+	public String getType()
+	{
+		return "ICBMLauncher";
+	}
 
-    @Override
-    public String[] getMethodNames()
-    {
-        return new String[] { "launch", "getTarget", "setTarget", "canLaunch", "setFrequency", "getFrequency", "getMissile" };
-    }
+	@Override
+	public String[] getMethodNames()
+	{
+		return new String[] { "launch", "getTarget", "setTarget", "canLaunch", "setFrequency", "getFrequency", "getMissile" };
+	}
 
-    @Override
-    public Object[] callMethod(IComputerAccess computer, ILuaContext context, int method, Object[] arguments) throws Exception
-    {
-        switch (method)
-        {
-            case 0:
-                this.launch();
-                return null;
-            case 1:
-                return new Object[] { this.getTarget().x, this.getTarget().y, this.getTarget().z };
-            case 2:
-                if (arguments[0] != null && arguments[1] != null && arguments[2] != null)
-                {
-                    try
-                    {
-                        this.setTarget(new Vector3(((Double) arguments[0]).doubleValue(), ((Double) arguments[1]).doubleValue(), ((Double) arguments[2]).doubleValue()));
-                    }
-                    catch (Exception e)
-                    {
-                        e.printStackTrace();
-                        throw new Exception("Target Parameter is Invalid.");
-                    }
-                }
-                return null;
-            case 3:
-                return new Object[] { this.canLaunch() };
-            case 4:
-                if (arguments[0] != null)
-                {
-                    try
-                    {
-                        double arg = ((Double) arguments[0]).doubleValue();
-                        arg = Math.max(Math.min(arg, Short.MAX_VALUE), Short.MIN_VALUE);
-                        this.setFrequency((short) arg);
-                    }
-                    catch (Exception e)
-                    {
-                        e.printStackTrace();
-                        throw new Exception("Frequency Parameter is Invalid.");
-                    }
-                }
-                return null;
-            case 5:
-                return new Object[] { this.getFrequency() };
-            case 6:
-                if (this.getMissile() != null)
-                {
-                    return new Object[] { this.getMissile().getExplosiveType().getMissileName() };
-                }
-                else
-                {
-                    return null;
-                }
-        }
+	@Override
+	public Object[] callMethod(IComputerAccess computer, ILuaContext context, int method, Object[] arguments) throws Exception
+	{
+		switch (method)
+		{
+			case 0:
+				this.launch();
+				return null;
+			case 1:
+				return new Object[] { this.getTarget().x, this.getTarget().y, this.getTarget().z };
+			case 2:
+				if (arguments[0] != null && arguments[1] != null && arguments[2] != null)
+				{
+					try
+					{
+						this.setTarget(new Vector3(((Double) arguments[0]).doubleValue(), ((Double) arguments[1]).doubleValue(), ((Double) arguments[2]).doubleValue()));
+					}
+					catch (Exception e)
+					{
+						e.printStackTrace();
+						throw new Exception("Target Parameter is Invalid.");
+					}
+				}
+				return null;
+			case 3:
+				return new Object[] { this.canLaunch() };
+			case 4:
+				if (arguments[0] != null)
+				{
+					try
+					{
+						double arg = ((Double) arguments[0]).doubleValue();
+						arg = Math.max(Math.min(arg, Short.MAX_VALUE), Short.MIN_VALUE);
+						this.setFrequency((short) arg);
+					}
+					catch (Exception e)
+					{
+						e.printStackTrace();
+						throw new Exception("Frequency Parameter is Invalid.");
+					}
+				}
+				return null;
+			case 5:
+				return new Object[] { this.getFrequency() };
+			case 6:
+				if (this.getMissile() != null)
+				{
+					return new Object[] { this.getMissile().getExplosiveType().getMissileName() };
+				}
+				else
+				{
+					return null;
+				}
+		}
 
-        throw new Exception("Invalid ICBM Launcher Function.");
-    }
+		throw new Exception("Invalid ICBM Launcher Function.");
+	}
 
-    @Override
-    public boolean canAttachToSide(int side)
-    {
-        return true;
-    }
+	@Override
+	public boolean canAttachToSide(int side)
+	{
+		return true;
+	}
 
-    @Override
-    public void attach(IComputerAccess computer)
-    {
+	@Override
+	public void attach(IComputerAccess computer)
+	{
 
-    }
+	}
 
-    @Override
-    public void detach(IComputerAccess computer)
-    {
+	@Override
+	public void detach(IComputerAccess computer)
+	{
 
-    }
+	}
 
-    @Override
-    public void readFromNBT(NBTTagCompound nbt)
-    {
-        super.readFromNBT(nbt);
-        this.targetPos = new Vector3(nbt.getCompoundTag("target"));
-    }
+	@Override
+	public void readFromNBT(NBTTagCompound nbt)
+	{
+		super.readFromNBT(nbt);
+		this.targetPos = new Vector3(nbt.getCompoundTag("target"));
+	}
 
-    @Override
-    public void writeToNBT(NBTTagCompound nbt)
-    {
-        super.writeToNBT(nbt);
+	@Override
+	public void writeToNBT(NBTTagCompound nbt)
+	{
+		super.writeToNBT(nbt);
 
-        if (this.targetPos != null)
-        {
-            nbt.setCompoundTag("target", this.targetPos.writeToNBT(new NBTTagCompound()));
-        }
-    }
+		if (this.targetPos != null)
+		{
+			nbt.setCompoundTag("target", this.targetPos.writeToNBT(new NBTTagCompound()));
+		}
+	}
 }
