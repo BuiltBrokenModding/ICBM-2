10fd433deac9894a00269b5417eb64928f39a2c3
Calclavia <admin@calclavia.com>
2014-01-25 17:11:18 +0800

    Updated UE

--- src/main/java/icbm/sentry/turret/Sentry.java
+++ src/main/java/icbm/sentry/turret/Sentry.java
@@ -2,126 +2,129 @@ package icbm.sentry.turret;
 
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
 import universalelectricity.api.energy.EnergyStorageHandler;
-import universalelectricity.api.energy.IEntityEnergyContainer;
+import universalelectricity.api.energy.IEnergyContainer;
 import universalelectricity.api.vector.Vector3;
 import calclavia.lib.utility.nbt.ISaveObj;
 import calclavia.lib.utility.nbt.SaveManager;
 
-/** Module way to deal with sentry guns
+/**
+ * Module way to deal with sentry guns
  * 
- * @author DarkGuardsman */
-public class Sentry implements IEntityEnergyContainer, ISaveObj
+ * @author DarkGuardsman
+ */
+public class Sentry implements IEnergyContainer, ISaveObj
 {
-    protected Vector3 aimOffset = new Vector3(1, 0, 0);
-    protected Vector3 centerOffset = new Vector3(0.5, 0.5, 0.5);
-    public TileSentry host;
-    protected float health = 0;
-    protected float maxHealth = -1;
-    protected EnergyStorageHandler energy;
+	protected Vector3 aimOffset = new Vector3(1, 0, 0);
+	protected Vector3 centerOffset = new Vector3(0.5, 0.5, 0.5);
+	public TileSentry host;
+	protected float health = 0;
+	protected float maxHealth = -1;
+	protected EnergyStorageHandler energy;
 
-    public Sentry(TileSentry host)
-    {
-        this.host = host;
-        this.energy = new EnergyStorageHandler(1000);
-    }
+	public Sentry(TileSentry host)
+	{
+		this.host = host;
+		this.energy = new EnergyStorageHandler(1000);
+	}
 
-    public void update()
-    {
+	public void update()
+	{
 
-    }
+	}
 
-    public boolean canFire()
-    {
-        return false;
-    }
+	public boolean canFire()
+	{
+		return false;
+	}
 
-    public boolean fireWeapon(Vector3 target)
-    {
-        return false;
-    }
+	public boolean fireWeapon(Vector3 target)
+	{
+		return false;
+	}
 
-    public void fireWeaponClient(Vector3 target)
-    {
-        // TODO Auto-generated method stub
+	public void fireWeaponClient(Vector3 target)
+	{
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    public Vector3 getAimOffset()
-    {
-        return this.aimOffset;
-    }
+	public Vector3 getAimOffset()
+	{
+		return this.aimOffset;
+	}
 
-    public Vector3 getCenterOffset()
-    {
-        return this.centerOffset;
-    }
+	public Vector3 getCenterOffset()
+	{
+		return this.centerOffset;
+	}
 
-    public float getHealth()
-    {
-        return this.health;
-    }
+	public float getHealth()
+	{
+		return this.health;
+	}
 
-    public float getMaxHealth()
-    {
-        return this.maxHealth;
-    }
+	public float getMaxHealth()
+	{
+		return this.maxHealth;
+	}
 
-    @Override
-    public void setEnergy(long energy)
-    {
-        this.energy.setEnergy(energy);
-    }
+	@Override
+	public void setEnergy(ForgeDirection dir, long energy)
+	{
+		this.energy.setEnergy(energy);
+	}
 
-    @Override
-    public long getEnergy()
-    {
-        return this.energy.getEnergy();
-    }
+	@Override
+	public long getEnergy(ForgeDirection dir)
+	{
+		return this.energy.getEnergy();
+	}
 
-    @Override
-    public long getEnergyCapacity()
-    {
-        return this.energy.getEnergyCapacity();
-    }
+	@Override
+	public long getEnergyCapacity(ForgeDirection dir)
+	{
+		return this.energy.getEnergyCapacity();
+	}
 
-    @Override
-    public void save(NBTTagCompound nbt)
-    {
-        nbt.setString("id", SaveManager.getID(this.getClass()));
-        if (this.energy != null)
-            this.energy.writeToNBT(nbt);
-        if (this.maxHealth > 0)
-            nbt.setFloat("Health", this.health);
+	@Override
+	public void save(NBTTagCompound nbt)
+	{
+		nbt.setString("id", SaveManager.getID(this.getClass()));
+		if (this.energy != null)
+			this.energy.writeToNBT(nbt);
+		if (this.maxHealth > 0)
+			nbt.setFloat("Health", this.health);
 
-    }
+	}
 
-    @Override
-    public void load(NBTTagCompound nbt)
-    {
-        if (this.energy != null)
-            this.energy.readFromNBT(nbt);
-        this.health = nbt.getFloat("Health");
+	@Override
+	public void load(NBTTagCompound nbt)
+	{
+		if (this.energy != null)
+			this.energy.readFromNBT(nbt);
+		this.health = nbt.getFloat("Health");
 
-    }
+	}
 
-    public World world()
-    {
-        return this.host.worldObj;
-    }
+	public World world()
+	{
+		return this.host.worldObj;
+	}
 
-    public double x()
-    {
-        return this.host.xCoord;
-    }
+	public double x()
+	{
+		return this.host.xCoord;
+	}
 
-    public double y()
-    {
-        return this.host.yCoord;
-    }
+	public double y()
+	{
+		return this.host.yCoord;
+	}
 
-    public double z()
-    {
-        return this.host.zCoord;
-    }
+	public double z()
+	{
+		return this.host.zCoord;
+	}
 }
