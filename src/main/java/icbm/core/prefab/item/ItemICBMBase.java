fd5e2461565819f4b63de6f6fa873f7c9ee71dc8
DarkGaurdsman <rseifert.phone@gmail.com>
2014-01-31 14:41:31 -0500

    Added an overload to ItemICBMBase

--- src/main/java/icbm/core/prefab/item/ItemICBMBase.java
+++ src/main/java/icbm/core/prefab/item/ItemICBMBase.java
@@ -5,8 +5,16 @@ import icbm.core.CreativeTabICBM;
 import icbm.core.Settings;
 import net.minecraft.item.Item;
 
+/** Prefab for ICBM items that sets the creative tab, texture name, and translation name
+ * 
+ * @author DarkGuardsman */
 public class ItemICBMBase extends Item
 {
+    public ItemICBMBase(String name)
+    {
+        this(Settings.getNextItemID(), name);
+    }
+
     public ItemICBMBase(int id, String name)
     {
         super(Settings.CONFIGURATION.getItem(name, id).getInt(id));
