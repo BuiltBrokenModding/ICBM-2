e4bcfb2e29b621a4de4166f9b108ef81bcdcd922
tgame14 <tgameBusiness@gmail.com>
2014-02-16 17:46:48 +0200

    fixed missing implementation

--- sentry/src/main/java/icbm/sentry/turret/weapon/WeaponLaser.java
+++ sentry/src/main/java/icbm/sentry/turret/weapon/WeaponLaser.java
@@ -1,6 +1,7 @@
 package icbm.sentry.turret.weapon;
 
 import icbm.sentry.turret.Sentry;
+import universalelectricity.api.vector.VectorWorld;
 
 /**
  * @since 16/02/14
@@ -11,5 +12,11 @@ public class WeaponLaser extends WeaponSystem
     public WeaponLaser (Sentry sentry)
     {
         super(sentry);
+    }
+
+    @Override
+    public void fire (VectorWorld target)
+    {
+        
     }
 }
