85a6dee4d43fe92cbf3b83af0e0710cd5cf4f543
DarkGuardsman <rseifert.phone@gmail.com>
2014-02-18 16:51:11 -0500

    Added to sentry targeting ray trace

--- sentry/src/main/java/icbm/sentry/turret/ai/SentryAI.java
+++ sentry/src/main/java/icbm/sentry/turret/ai/SentryAI.java
@@ -8,6 +8,7 @@ import net.minecraft.entity.INpc;
 import net.minecraft.entity.passive.IAnimals;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.EnumMovingObjectType;
 import net.minecraft.util.MovingObjectPosition;
 import universalelectricity.api.vector.Vector3;
 import universalelectricity.api.vector.VectorWorld;
@@ -33,34 +34,42 @@ public class SentryAI
     {
         if (container != null && container.getSentry() != null && container.getSentry().automated())
         {
-            //For the moment work on AI code inside this class
-            
+            //Find target if we have none
             if (target == null)
             {
                 target = findTarget(container.getSentry(), null, 100);
                 return;
             }
-            //TODO: if target &&  not facing, update rotation
-            else
-            {
-                //TODO get position of sentry and of target. Then check if delta angle is +-2.3 degrees
-
-                MovingObjectPosition endTarget = this.container.getSentry().getAimOffset().rayTraceEntities(this.container.world(), Vector3.fromCenter(this.target));
-                if (endTarget != null)
+            //if we have a target start doing rotation, and line of sight checks
+            if (target != null)
+            {               
+                Vector3 barrel = new Vector3();
+                barrel.add(this.container.getSentry().getCenterOffset());
+                barrel.add(this.container.getSentry().getAimOffset());
+                barrel.rotate(this.container.yaw(), this.container.pitch());
+                barrel.add(new Vector3(container.x(), container.y(), container.z()));
+                
+                MovingObjectPosition endTarget = barrel.rayTrace(this.container.world(), Vector3.fromCenter(this.target), true);
+                //This ray trace is just for line of sight
+                if (endTarget != null && endTarget.typeOfHit == EnumMovingObjectType.ENTITY)
                 {
-                    // Assuming the Vec3 Ray tracing includes the current pitch and angle given to the Sentry obj
+                    //TODO change getAngle out for the correct version
+                    double deltaYaw = barrel.getAngle(new Vector3(target));
+                    double deltaPitch = barrel.getAngle(new Vector3(target));
+                    //TODO get position of sentry and of target. Then check if delta angle is +-2.3 degrees
+                    
                     this.container.getSentry().fire();
                 }
                 //TODO: if target && aimed at target && can fire, fire weapon
                 // -- Might be able to split of Ability to fire to the Sentry obj itself? have the AI only fire commands.
             }
-            
+
         }
     }
 
     @SuppressWarnings("unchecked")
     protected EntityLivingBase findTarget(ISentry sentry, IEntitySelector targetSelector, int range)
-    {      
+    {
         List<EntityLivingBase> list = container.world().selectEntitiesWithinAABB(EntityLivingBase.class, AxisAlignedBB.getBoundingBox(container.x() + sentry.getCenterOffset().x, container.y() + sentry.getCenterOffset().y, container.z() + sentry.getCenterOffset().z, container.x() + sentry.getCenterOffset().x, container.y() + sentry.getCenterOffset().y, container.z() + sentry.getCenterOffset().z).expand(range, range, range), targetSelector);
         Collections.sort(list, new ComparatorClosestEntity(new VectorWorld(container.world(), container.x() + sentry.getCenterOffset().x, container.y() + sentry.getCenterOffset().y, container.z() + sentry.getCenterOffset().z)));
         if (list != null && !list.isEmpty())
@@ -114,7 +123,7 @@ public class SentryAI
         {
             double distanceA = this.location.distance(entityA);
             double distanceB = this.location.distance(entityB);
-            if(Math.abs(distanceA - distanceB) < 1.5)
+            if (Math.abs(distanceA - distanceB) < 1.5)
             {
                 float healthA = entityA.getHealth();
                 float healthB = entityB.getHealth();
