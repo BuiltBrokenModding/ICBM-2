db2615675f3ffc407d8e540bf532d6db30fbc100
tgame14 <tgameBusiness@gmail.com>
2014-02-23 11:37:57 +0200

    Modules now support late load phase registrations

--- src/main/java/icbm/core/ProxyHandler.java
+++ src/main/java/icbm/core/ProxyHandler.java
@@ -1,14 +1,25 @@
 package icbm.core;
 
+import calclavia.lib.modproxy.IIntegrationProxy;
 import calclavia.lib.modproxy.IMod;
-import calclavia.lib.modproxy.IProxy;
 import calclavia.lib.modproxy.LoadPhase;
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 import java.util.LinkedList;
 import java.util.List;
 
 /**
  * the Class that handles the submods of All ICBM
+ *
+ * to have the submodules work, You must register them in this class,
+ * Adding support for a submodule includes only aquiring its class and throwing it in the
+ * registerModules method, this is handled as such to allow turning these modules off by configuration,
+ * and disable them if the parent mod is not loaded (Integration modules with other mods)
+ *
+ * Replace @Mod annotation with this system and it allows better handling in the end of it
  *
  * @since 23/02/14
  * @author tgame14
@@ -17,16 +28,23 @@ public class ProxyHandler
 {
 
     private static List<IMod> submodList = new LinkedList<IMod>();
-    private static List<IProxy> compatModulesList = new LinkedList<IProxy>();
+    private static List<IIntegrationProxy> compatModulesList = new LinkedList<IIntegrationProxy>();
     private static LoadPhase phase = LoadPhase.PRELAUNCH;
 
-    private void applyModules(Class<?> clazz, String modId) throws IllegalAccessException, InstantiationException
+    public static void registerModules ()
+    {
+
+    }
+
+    private void applyModules (Class<?> clazz, boolean load) throws IllegalAccessException, InstantiationException
     {
         if (clazz.isAssignableFrom(IMod.class))
         {
-            if (clazz.isAssignableFrom(IProxy.class))
+            if (clazz.isAssignableFrom(IIntegrationProxy.class))
             {
-                compatModulesList.add((IProxy) clazz.newInstance());
+                IIntegrationProxy proxy = (IIntegrationProxy) clazz.newInstance();
+                if (Loader.isModLoaded(proxy.modId()))
+                    compatModulesList.add(proxy);
             }
 
             else
@@ -36,10 +54,87 @@ public class ProxyHandler
         }
     }
 
-
-
-    public static void registerModules()
+    /** Call for modules late or as already existing modules, DO NOT CALL FOR REGISTERED Proxies! */
+    public void applyModules (IIntegrationProxy module)
     {
+        boolean registered = false;
+
+        if (Loader.isModLoaded(module.modId()))
+        {
+            compatModulesList.add((IIntegrationProxy) module);
+            registered = true;
+        }
+
+        if (registered)
+        {
+            switch (phase)
+            {
+            case DONE:
+                break;
+            case POSTINIT:
+                module.preInit();
+                module.init();
+                module.preInit();
+                break;
+            case INIT:
+                module.preInit();
+                module.init();
+                break;
+            case PREINIT:
+                module.preInit();
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    public void preInit (FMLPreInitializationEvent event)
+    {
+        phase = LoadPhase.PREINIT;
+
+        for (IMod submod : submodList)
+        {
+            submod.preInit(event);
+        }
+
+        for (IIntegrationProxy proxy : compatModulesList)
+        {
+            proxy.preInit();
+        }
 
     }
+
+    public void init (FMLInitializationEvent event)
+    {
+        phase = LoadPhase.INIT;
+
+        for (IMod submod : submodList)
+        {
+            submod.init(event);
+        }
+
+        for (IIntegrationProxy proxy : compatModulesList)
+        {
+            proxy.init();
+        }
+    }
+
+    public void postInit (FMLPostInitializationEvent event)
+    {
+        phase = LoadPhase.POSTINIT;
+
+        for (IMod submod : submodList)
+        {
+            submod.postInit(event);
+        }
+
+        for (IIntegrationProxy proxy : compatModulesList)
+        {
+            proxy.postInit();
+        }
+
+        phase = LoadPhase.DONE;
+    }
+
 }
