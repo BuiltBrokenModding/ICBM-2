51e08b3734ea454bbc4bf6192f039e6e212019b4
Robert Seifert <rseifert.phone@gmail.com>
2014-03-10 00:48:17 -0400

    Changed weapon inaccuracy to work similar falloff

--- sentry/src/main/java/icbm/sentry/turret/weapon/WeaponProjectile.java
+++ sentry/src/main/java/icbm/sentry/turret/weapon/WeaponProjectile.java
@@ -1,5 +1,7 @@
 package icbm.sentry.turret.weapon;
 
+import java.util.Random;
+
 import icbm.api.sentry.IAmmunition;
 import icbm.api.sentry.ProjectileType;
 import icbm.sentry.ICBMSentry;
@@ -10,93 +12,98 @@ import net.minecraft.item.ItemStack;
 import universalelectricity.api.vector.Vector3;
 import calclavia.lib.utility.inventory.InventoryUtility;
 
-/**
- * Basic projectile weapon system design more to be used as a prefab. By default it acts like a hand
+/** Basic projectile weapon system design more to be used as a prefab. By default it acts like a hand
  * gun with low hit chance and damage.
  * 
- * @author DarkGuardsman, tgame14, Calclavia
- */
+ * @author DarkGuardsman, tgame14, Calclavia */
 public class WeaponProjectile extends WeaponDamage
 {
-	protected float inaccuracy = 0.1f;
-	protected int ammoAmount;
+    protected float inaccuracy = 0.0005f;
+    protected float min_range = 1;
+    protected float max_range = 100;
+    protected int ammoAmount;
 
-	public WeaponProjectile(Turret sentry, int ammoAmount, float damage)
-	{
-		super(sentry, TurretDamageSource.turretProjectile, damage);
-		this.ammoAmount = ammoAmount;
-	}
+    public WeaponProjectile(Turret sentry, int ammoAmount, float damage)
+    {
+        super(sentry, TurretDamageSource.turretProjectile, damage);
+        this.ammoAmount = ammoAmount;
+    }
 
-	@Override
-	public void fire(Vector3 target)
-	{
-		super.fire(target.clone().translate(getInaccuracy(), getInaccuracy(), getInaccuracy()));
-		consumeAmmo(ammoAmount, true);
-	}
+    @Override
+    public void fire(Vector3 target)
+    {
+        double d = target.distance(this.turret.getAbsoluteCenter());
+        super.fire(target.clone().translate(getInaccuracy(d), getInaccuracy(d), getInaccuracy(d)));
+        consumeAmmo(ammoAmount, true);
+    }
 
-	private float getInaccuracy()
-	{
-		return turret.getHost().world().rand.nextFloat() * inaccuracy;
-	}
+    private double getInaccuracy(double distance)
+    {
+        Random rand = turret.getHost().world().rand;
+        double offset = distance * (rand.nextFloat() - rand.nextFloat()) * inaccuracy;
+        if (distance < min_range || distance > max_range)
+        {
+            return offset * 2;
+        }
+        return offset;
+    }
 
-	public boolean isAmmo(ItemStack stack)
-	{
-		return stack != null && stack.getItem() instanceof IAmmunition && ((IAmmunition) stack.getItem()).getType(stack) == ProjectileType.CONVENTIONAL;
-	}
+    public boolean isAmmo(ItemStack stack)
+    {
+        return stack != null && stack.getItem() instanceof IAmmunition && ((IAmmunition) stack.getItem()).getType(stack) == ProjectileType.CONVENTIONAL;
+    }
 
-	@Override
-	public boolean canFire()
-	{
-		return consumeAmmo(ammoAmount, false);
-	}
+    @Override
+    public boolean canFire()
+    {
+        return consumeAmmo(ammoAmount, false);
+    }
 
-	/**
-	 * Used to consume ammo or check if ammo can be consumed
-	 * 
-	 * @param count - number of items to consume
-	 * @param doConsume - true items will be consumed
-	 * @return true if all rounds were consumed
-	 */
-	public boolean consumeAmmo(int count, boolean doConsume)
-	{
-		int consumeCount = 0;
-		int need = count;
-		IInventory inv = turret.getHost().getInventory();
+    /** Used to consume ammo or check if ammo can be consumed
+     * 
+     * @param count - number of items to consume
+     * @param doConsume - true items will be consumed
+     * @return true if all rounds were consumed */
+    public boolean consumeAmmo(int count, boolean doConsume)
+    {
+        int consumeCount = 0;
+        int need = count;
+        IInventory inv = turret.getHost().getInventory();
 
-		if (count > 0 && inv != null)
-		{
-			for (int slot = 0; slot < inv.getSizeInventory(); slot++)
-			{
-				ItemStack itemStack = inv.getStackInSlot(slot);
+        if (count > 0 && inv != null)
+        {
+            for (int slot = 0; slot < inv.getSizeInventory(); slot++)
+            {
+                ItemStack itemStack = inv.getStackInSlot(slot);
 
-				if (isAmmo(itemStack))
-				{
-					IAmmunition ammo = (IAmmunition) itemStack.getItem();
-					if (ammo.getAmmoCount(itemStack) >= need)
-					{
-						if (doConsume)
-						{
-							inv.setInventorySlotContents(slot, ammo.consumeAmmo(itemStack, need));
-							InventoryUtility.dropItemStack(turret.world(), turret.getPosition(), ammo.getShell(itemStack, need));
-						}
-						return true;
-					}
-					else
-					{
-						int consume = need - ammo.getAmmoCount(itemStack);
-						if (doConsume)
-						{
-							inv.setInventorySlotContents(slot, ammo.consumeAmmo(itemStack, consume));
-							InventoryUtility.dropItemStack(turret.world(), turret.getPosition(), ammo.getShell(itemStack, consume));
+                if (isAmmo(itemStack))
+                {
+                    IAmmunition ammo = (IAmmunition) itemStack.getItem();
+                    if (ammo.getAmmoCount(itemStack) >= need)
+                    {
+                        if (doConsume)
+                        {
+                            inv.setInventorySlotContents(slot, ammo.consumeAmmo(itemStack, need));
+                            InventoryUtility.dropItemStack(turret.world(), turret.getPosition(), ammo.getShell(itemStack, need));
+                        }
+                        return true;
+                    }
+                    else
+                    {
+                        int consume = need - ammo.getAmmoCount(itemStack);
+                        if (doConsume)
+                        {
+                            inv.setInventorySlotContents(slot, ammo.consumeAmmo(itemStack, consume));
+                            InventoryUtility.dropItemStack(turret.world(), turret.getPosition(), ammo.getShell(itemStack, consume));
 
-						}
-						need -= consume;
-					}
-					consumeCount += ammo.getAmmoCount(itemStack);
-				}
-			}
-		}
+                        }
+                        need -= consume;
+                    }
+                    consumeCount += ammo.getAmmoCount(itemStack);
+                }
+            }
+        }
 
-		return consumeCount >= count;
-	}
+        return consumeCount >= count;
+    }
 }
