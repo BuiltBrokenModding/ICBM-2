fa4bb9d47b60b7f1de81e292171187213edfcc71
Robert Seifert <rseifert.phone@gmail.com>
2014-03-10 01:19:18 -0400

    Added a fire method without target for mounted sentries

--- sentry/src/main/java/icbm/sentry/turret/mounted/TurretMounted.java
+++ sentry/src/main/java/icbm/sentry/turret/mounted/TurretMounted.java
@@ -4,59 +4,73 @@ import icbm.sentry.interfaces.ITurretProvider;
 import icbm.sentry.turret.EntityMountableDummy;
 import icbm.sentry.turret.Turret;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.MovingObjectPosition;
 import universalelectricity.api.vector.EulerAngle;
 import universalelectricity.api.vector.Vector3;
 
 /** this sentry uses for mounting the player in position */
 public abstract class TurretMounted extends Turret
 {
-	protected Vector3 riderOffset = new Vector3();
+    protected Vector3 riderOffset = new Vector3();
 
-	public TurretMounted(ITurretProvider turretProvider)
-	{
-		super(turretProvider);
-	}
+    public TurretMounted(ITurretProvider turretProvider)
+    {
+        super(turretProvider);
+    }
 
-	@Override
-	public void update()
-	{
-		super.update();
+    @Override
+    public void update()
+    {
+        super.update();
 
-		if (!world().isRemote)
-		{
-			EntityMountableDummy sentryEntity = getHost().getFakeEntity();
+        if (!world().isRemote)
+        {
+            EntityMountableDummy sentryEntity = getHost().getFakeEntity();
 
-			if (sentryEntity.riddenByEntity instanceof EntityPlayer)
-			{
-				EntityPlayer mountedPlayer = (EntityPlayer) sentryEntity.riddenByEntity;
-				getServo().yaw = -mountedPlayer.rotationYaw + 180;
-				getServo().pitch = -mountedPlayer.rotationPitch;
-				getServo().update();
-			}
+            if (sentryEntity.riddenByEntity instanceof EntityPlayer)
+            {
+                EntityPlayer mountedPlayer = (EntityPlayer) sentryEntity.riddenByEntity;
+                getServo().yaw = -mountedPlayer.rotationYaw + 180;
+                getServo().pitch = -mountedPlayer.rotationPitch;
+                getServo().update();
+            }
 
-			if (world().isBlockIndirectlyGettingPowered((int) getHost().x(), (int) getHost().y() - 1, (int) getHost().z()))
-			{
-				if (canFire())
-				{
-					tryFire();
-				}
-			}
-		}
-	}
+            if (world().isBlockIndirectlyGettingPowered((int) getHost().x(), (int) getHost().y() - 1, (int) getHost().z()))
+            {
+                if (canFire())
+                {
+                    tryFire();
+                }
+            }
+        }
+    }
 
-	public void tryFire()
-	{
+    public void fire()
+    {
+        Vector3 start = this.getAbsoluteCenter().translate(this.getAimOffset());
+        Vector3 end = this.getAbsoluteCenter().translate(this.getAimOffset().scale(500));
+        MovingObjectPosition endTarget = start.rayTrace(world(), end, true);
+        Vector3 hit = end;
+        if (endTarget != null)
+        {
+            hit = new Vector3(endTarget);
+        }
+        fire(hit);
+    }
 
-	}
+    public void tryFire()
+    {
 
-	public Vector3 getRiderOffset()
-	{
-		return riderOffset;
-	}
+    }
 
-	@Override
-	public boolean mountable()
-	{
-		return true;
-	}
+    public Vector3 getRiderOffset()
+    {
+        return riderOffset;
+    }
+
+    @Override
+    public boolean mountable()
+    {
+        return true;
+    }
 }
