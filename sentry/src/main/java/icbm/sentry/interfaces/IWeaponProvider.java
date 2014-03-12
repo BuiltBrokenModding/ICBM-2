8e797d15455955c9765f15ef549677ac5fe0f924
Robert Seifert <rseifert.phone@gmail.com>
2014-03-12 04:11:12 -0400

    Created a weapon provider interface

---
+++ sentry/src/main/java/icbm/sentry/interfaces/IWeaponProvider.java
@@ -0,0 +1,9 @@
+package icbm.sentry.interfaces;
+
+import icbm.sentry.turret.weapon.IWeaponSystem;
+
+/** @author DarkGuardsman */
+public interface IWeaponProvider
+{
+    public IWeaponSystem getWeaponSystem();
+}
