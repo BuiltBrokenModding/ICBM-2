fbfc07eb2442f8891e2d6322fed3a8aff5ea566d
Robert Seifert <rseifert.phone@gmail.com>
2014-03-14 16:31:05 -0400

    Fixed missing cornered rail texture

--- src/main/java/icbm/contraption/block/BlockReinforcedRail.java
+++ src/main/java/icbm/contraption/block/BlockReinforcedRail.java
@@ -1,6 +1,5 @@
 package icbm.contraption.block;
 
-import icbm.Reference;
 import net.minecraft.block.BlockRail;
 import net.minecraft.entity.item.EntityMinecart;
 import net.minecraft.world.World;
@@ -8,17 +7,17 @@ import net.minecraft.world.World;
 /** @author DarkGuardsman */
 public class BlockReinforcedRail extends BlockRail
 {
-	public BlockReinforcedRail(int id)
-	{
-		super(id);
-		setHardness(10F);
-		setResistance(10F);
-		setStepSound(soundMetalFootstep);
-	}
+    public BlockReinforcedRail(int id)
+    {
+        super(id);
+        setHardness(10F);
+        setResistance(10F);
+        setStepSound(soundMetalFootstep);
+    }
 
-	@Override
-	public float getRailMaxSpeed(World world, EntityMinecart cart, int y, int x, int z)
-	{
-		return 0.6f;
-	}
+    @Override
+    public float getRailMaxSpeed(World world, EntityMinecart cart, int y, int x, int z)
+    {
+        return 0.6f;
+    }
 }
