94a275744b9faaa76feeb750d133282466155a22
Robert S <rseifert.phone@gmail.com>
2014-03-23 23:35:30 -0400

    Changed railgun weapon system to stop at blocks with over 100K hardnesss

--- sentry/src/main/java/icbm/sentry/turret/weapon/WeaponRailgun.java
+++ sentry/src/main/java/icbm/sentry/turret/weapon/WeaponRailgun.java
@@ -15,91 +15,98 @@ import calclavia.api.icbm.sentry.IAmmunition;
 import calclavia.api.icbm.sentry.ProjectileType;
 import calclavia.lib.prefab.vector.Cuboid;
 
-/**
- * High powered electro magnetic cannon designed to throw a small metal object up to sonic speeds
+/** High powered electro magnetic cannon designed to throw a small metal object up to sonic speeds
  * 
- * @author Darkguardsman
- */
+ * @author Darkguardsman */
 public class WeaponRailgun extends WeaponProjectile implements IEnergyWeapon
 {
-	private long energyCost = 1000000;
+    private long energyCost = 1000000;
 
-	public WeaponRailgun(Turret sentry)
-	{
-		this(sentry, 100);
-	}
+    public WeaponRailgun(Turret sentry)
+    {
+        this(sentry, 100);
+    }
 
-	public WeaponRailgun(Turret sentry, float damage)
-	{
-		super(sentry, 1, damage);
-	}
+    public WeaponRailgun(Turret sentry, float damage)
+    {
+        super(sentry, 1, damage);
+    }
 
-	public WeaponRailgun(Turret sentry, float damage, long energy)
-	{
-		this(sentry, damage);
-		this.energyCost = energy;
-	}
+    public WeaponRailgun(Turret sentry, float damage, long energy)
+    {
+        this(sentry, damage);
+        this.energyCost = energy;
+    }
 
-	@Override
-	public void onHitEntity(Entity entity)
-	{
-		super.onHitEntity(entity);
-		onHitBlock(Vector3.fromCenter(entity));
-	}
+    @Override
+    public void onHitEntity(Entity entity)
+    {
+        super.onHitEntity(entity);
+        onHitBlock(Vector3.fromCenter(entity));
+    }
 
-	@SuppressWarnings("unused")
-	@Override
-	public void onHitBlock(Vector3 hit)
-	{
-		int size = 10;
+    @SuppressWarnings("unused")
+    @Override
+    public void onHitBlock(Vector3 hit)
+    {
+        int size = 10;
 
-		/** Kill all active explosives with antimatter. */
-		if (false)
-		{
-			AxisAlignedBB bounds = new Cuboid().expand(50).translate(hit).toAABB();
-			List<IEntityExplosion> entities = this.turret.world().getEntitiesWithinAABB(IEntityExplosion.class, bounds);
+        /** Kill all active explosives with antimatter. */
+        if (false)
+        {
+            AxisAlignedBB bounds = new Cuboid().expand(50).translate(hit).toAABB();
+            List<IEntityExplosion> entities = this.turret.world().getEntitiesWithinAABB(IEntityExplosion.class, bounds);
 
-			for (IEntityExplosion entity : entities)
-			{
-				entity.endExplosion();
-			}
-			size = 20;
-		}
+            for (IEntityExplosion entity : entities)
+            {
+                entity.endExplosion();
+            }
+            size = 20;
+        }
 
-		// TODO: Fix this null.
-		this.turret.world().newExplosion((Entity) null, hit.x, hit.y, hit.z, size, true, true);
+        // TODO: Fix this null.
+        this.turret.world().newExplosion((Entity) null, hit.x, hit.y, hit.z, size, true, true);
 
-		Block block = Block.blocksList[this.turret.world().getBlockId(hit.intX(), hit.intY(), hit.intZ())];
-		if (block != null && block.getBlockHardness(this.turret.world(), hit.intX(), hit.intY(), hit.intZ()) >= 0)
-		{
-			this.turret.world().setBlockToAir(hit.intX(), hit.intY(), hit.intZ());
-		}
-	}
+        Block block = Block.blocksList[this.turret.world().getBlockId(hit.intX(), hit.intY(), hit.intZ())];
+        if (block != null)
+        {
+            if (block.getBlockHardness(this.turret.world(), hit.intX(), hit.intY(), hit.intZ()) >= 0)
+            {
+                return;
+            }
+            else if (block.getBlockHardness(this.turret.world(), hit.intX(), hit.intY(), hit.intZ()) < 100000)
+            {
+                this.turret.world().setBlockToAir(hit.intX(), hit.intY(), hit.intZ());
+            }
 
-	@Override
-	public boolean isAmmo(ItemStack stack)
-	{
-		return super.isAmmo(stack) && ((IAmmunition) stack.getItem()).getType(stack) == ProjectileType.RAILGUN;
-	}
+        }
+    }
 
-	@Override
-	public void fire(Vector3 target)
-	{
-		double d = target.distance(this.turret.getAbsoluteCenter());
-		Vector3 normalized = target.clone().subtract(turret.getAbsoluteCenter()).normalize();
-		target.translate(normalized.scale(8));
+    @Override
+    public boolean isAmmo(ItemStack stack)
+    {
+        return super.isAmmo(stack) && ((IAmmunition) stack.getItem()).getType(stack) == ProjectileType.RAILGUN;
+    }
 
-		for (int i = 0; i < 5; i++)
-		{
-			doFire(target.clone().translate(getInaccuracy(d), getInaccuracy(d), getInaccuracy(d)));
-		}
+    @Override
+    public void fire(Vector3 target)
+    {
+        double d = target.distance(this.turret.getAbsoluteCenter());
+        Vector3 normalized = target.clone().subtract(turret.getAbsoluteCenter()).normalize();
+        target.translate(normalized.scale(8));
 
-		consumeAmmo(ammoAmount, true);
-	}
+        //Loops several times to allow it to punch threw several blocks
+        for (int i = 0; i < 5; i++)
+        {
+            doFire(target.clone().translate(getInaccuracy(d), getInaccuracy(d), getInaccuracy(d)));
+        }
 
-	@Override
-	public long getEnergyPerShot()
-	{
-		return this.energyCost;
-	}
+        consumeAmmo(ammoAmount, true);
+    }
+
+    @Override
+    public long getEnergyPerShot()
+    {
+        return this.energyCost;
+    }
 }
