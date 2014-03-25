ee954bbeb8ab5cc2bc3a890c7b2ebbb5da7749a0
Robert S <rseifert.phone@gmail.com>
2014-03-25 14:53:25 -0400

    Added itemstack saving to block projectile

--- sentry/src/main/java/icbm/sentry/entities/EntityBlockProjectile.java
+++ sentry/src/main/java/icbm/sentry/entities/EntityBlockProjectile.java
@@ -3,6 +3,7 @@ package icbm.sentry.entities;
 import calclavia.lib.utility.inventory.InventoryUtility;
 import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.MovingObjectPosition;
@@ -29,15 +30,41 @@ public class EntityBlockProjectile extends EntityThrowable
     @Override
     protected void onImpact(MovingObjectPosition movingobjectposition)
     {
-        if (movingobjectposition != null && movingobjectposition.entityHit != null)
-        {
-            float damageFromMotion = MathHelper.sqrt_double(this.motionX * this.motionX + this.motionY * this.motionY + this.motionZ * this.motionZ);
-
-            movingobjectposition.entityHit.attackEntityFrom(DamageSource.fallingBlock, Math.max(damageFromMotion, 5));
-        }
         if (stack != null)
         {
+            if (movingobjectposition != null && movingobjectposition.entityHit != null)
+            {
+                float damageFromMotion = MathHelper.sqrt_double(this.motionX * this.motionX + this.motionY * this.motionY + this.motionZ * this.motionZ);
+
+                movingobjectposition.entityHit.attackEntityFrom(DamageSource.fallingBlock, Math.max(damageFromMotion, 5));
+            }
+
             InventoryUtility.dropItemStack(this.worldObj, this.posX, this.posY, this.posZ, stack, 10, 0);
+        }
+        this.setDead();
+    }
+
+    @Override
+    public void readEntityFromNBT(NBTTagCompound nbt)
+    {
+        super.readEntityFromNBT(nbt);
+        if (nbt.hasKey("Stack"))
+        {
+            this.stack = ItemStack.loadItemStackFromNBT(nbt.getCompoundTag("Stack"));
+        }
+        if (this.stack == null)
+        {
+            this.setDead();
+        }
+    }
+
+    @Override
+    public void writeEntityToNBT(NBTTagCompound nbt)
+    {
+        super.writeEntityToNBT(nbt);
+        if (this.stack != null)
+        {
+            nbt.setCompoundTag("Stack", this.stack.writeToNBT(new NBTTagCompound()));
         }
     }
 
