ed270ae3cbb7cbcfb6454bdf937e95af30bc4986
Robert S <rseifert.phone@gmail.com>
2014-04-25 13:22:16 -0400

    Added saltpeter, TODO implement in gunpowder recipe

--- src/main/java/icbm/core/items/ItemSulfurDust.java
+++ src/main/java/icbm/core/items/ItemSulfurDust.java
@@ -2,12 +2,47 @@ package icbm.core.items;
 
 import icbm.core.prefab.item.ItemICBMBase;
 
+import java.util.List;
+
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 public class ItemSulfurDust extends ItemICBMBase
 {
+    @SideOnly(Side.CLIENT)
+    Icon salt_icon;
 
     public ItemSulfurDust(int id)
     {
         super(id, "sulfur");
+        this.setHasSubtypes(true);
     }
 
+    @Override
+    public Icon getIconFromDamage(int meta)
+    {
+        if (meta == 1)
+        {
+            return salt_icon;
+        }
+        return super.getIconFromDamage(meta);
+    }
+
+    @Override
+    public void registerIcons(IconRegister iconRegister)
+    {
+        super.registerIcons(iconRegister);
+        this.salt_icon = iconRegister.registerIcon(this.getUnlocalizedName().replace("item.", "") + "saltpeter");
+    }
+
+    @Override
+    public void getSubItems(int par1, CreativeTabs par2CreativeTabs, List par3List)
+    {
+        super.getSubItems(par1, par2CreativeTabs, par3List);
+        par3List.add(new ItemStack(par1, 1, 1));
+    }
 }
