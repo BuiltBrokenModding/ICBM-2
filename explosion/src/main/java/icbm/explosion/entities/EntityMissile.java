586cc3d020d41d83205ad2e1e9a3338198d8dc74
Mike Liu <gravityreallyhatesme@gmail.com>
2014-05-02 00:35:01 +0000

    Fixed missile chunkloader bug that caused unpredictable chunkloading when leaving force loaded chunks.

--- explosion/src/main/java/icbm/explosion/entities/EntityMissile.java
+++ explosion/src/main/java/icbm/explosion/entities/EntityMissile.java
@@ -303,6 +303,10 @@ public class EntityMissile extends Entity implements IChunkLoadHandler, IExplosi
             loadedChunks.add(new ChunkCoordIntPair(newChunkX - 1, newChunkZ - 1));
             loadedChunks.add(new ChunkCoordIntPair(newChunkX + 1, newChunkZ - 1));
             loadedChunks.add(new ChunkCoordIntPair(newChunkX - 1, newChunkZ + 1));
+            loadedChunks.add(new ChunkCoordIntPair(newChunkX + 1, newChunkZ));
+            loadedChunks.add(new ChunkCoordIntPair(newChunkX, newChunkZ + 1));
+            loadedChunks.add(new ChunkCoordIntPair(newChunkX - 1, newChunkZ));
+            loadedChunks.add(new ChunkCoordIntPair(newChunkX, newChunkZ) - 1);
 
             for (ChunkCoordIntPair chunk : loadedChunks)
                 ForgeChunkManager.forceChunk(chunkTicket, chunk);
