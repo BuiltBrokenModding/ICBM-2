9ad2cacccf98a92643499a549a5f650d619908b6
Archtikz <atroglyphics@gmail.com>
2014-05-05 21:00:48 +0100

    Added some Decor Blocks, more colors through OpenGL coming soon.

--- src/main/java/icbm/core/blocks/BlockConcrete.java
+++ src/main/java/icbm/core/blocks/BlockConcrete.java
@@ -1,5 +1,6 @@
 package icbm.core.blocks;
 
+import icbm.Reference;
 import icbm.core.prefab.BlockICBM;
 
 import java.util.List;
@@ -17,8 +18,6 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockConcrete extends BlockICBM implements IAntiPoisonBlock
 {
-    private Icon iconCompact, iconReinforced;
-
     public BlockConcrete(int id)
     {
         super(id, "concrete", Material.rock);
@@ -30,15 +29,12 @@ public class BlockConcrete extends BlockICBM implements IAntiPoisonBlock
     @Override
     public Icon getIcon(int side, int metadata)
     {
-        switch (metadata)
-        {
-            case 1:
-                return this.iconCompact;
-            case 2:
-                return this.iconReinforced;
-        }
+    	 if (metadata < ConcreteType.values().length)
+         {
+             return ConcreteType.values()[metadata].icon;
+         }
 
-        return this.blockIcon;
+         return ConcreteType.CompactConcrete.icon;
     }
 
     @SideOnly(Side.CLIENT)
@@ -46,34 +42,27 @@ public class BlockConcrete extends BlockICBM implements IAntiPoisonBlock
     public void registerIcons(IconRegister iconRegister)
     {
         super.registerIcons(iconRegister);
+        for (ConcreteType data : ConcreteType.values())
+        {
+            data.icon = iconRegister.registerIcon(this.getUnlocalizedName().replace("tile.", "") + data.name);
+        }
 
-        this.iconCompact = iconRegister.registerIcon(this.getUnlocalizedName().replace("tile.", "") + "Compact");
-        this.iconReinforced = iconRegister.registerIcon(this.getUnlocalizedName().replace("tile.", "") + "Reinforced");
-
+        this.blockIcon = ConcreteType.CompactConcrete.icon;
     }
 
     @Override
     public float getExplosionResistance(Entity par1Entity, World world, int x, int y, int z, double explosionX, double explosionY, double explosionZ)
     {
-        int metadata = world.getBlockMetadata(x, y, z);
-
-        switch (metadata)
-        {
-            case 1:
-                return 38;
-            case 2:
-                return 48;
+        for(ConcreteType con : ConcreteType.values()) {
+        	return con.explores;
         }
-
         return this.getExplosionResistance(par1Entity);
     }
 
     @Override
-    public void getSubBlocks(int par1, CreativeTabs par2CreativeTabs, List par3List)
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            par3List.add(new ItemStack(par1, 1, i));
+    public void getSubBlocks(int par1, CreativeTabs par2CreativeTabs, List par3List) {
+        for(ConcreteType con : ConcreteType.values()) {
+            par3List.add(new ItemStack(par1, 1, con.ordinal()));
         }
     }
 
@@ -87,5 +76,22 @@ public class BlockConcrete extends BlockICBM implements IAntiPoisonBlock
     public boolean hasTileEntity(int metadata)
     {
         return false;
+    }
+    
+    public static enum ConcreteType {
+    	Concrete("_normal", 28),
+    	CompactConcrete("_compact", 38),
+    	ReinforcedConcrete("_reinforced", 48),
+        ColoredConcrete("_colored", 28),
+        DarkConcrete("_dark", 28);
+        
+        ConcreteType(String name, int explosionResis) {
+        	this.name = name;
+        	this.explores = explosionResis;
+        }
+        
+        public int explores;
+        public Icon icon;
+        public String name;
     }
 }
