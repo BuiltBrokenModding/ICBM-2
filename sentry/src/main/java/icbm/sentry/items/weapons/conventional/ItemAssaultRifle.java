5cd0e082f1355eb61b6d26784d11b0480b84dd97
Archtikz <atroglyphics@gmail.com>
2014-05-08 21:56:30 +0100

    Fix

--- sentry/src/main/java/icbm/sentry/items/weapons/conventional/ItemAssaultRifle.java
+++ sentry/src/main/java/icbm/sentry/items/weapons/conventional/ItemAssaultRifle.java
@@ -4,6 +4,6 @@ package icbm.sentry.items.weapons.conventional;
 public class ItemAssaultRifle extends ItemConventional {
 	
 	public ItemAssaultRifle(int id) {
-		super(id, 3, 1, 1, "machineGun");
+		super(id, 3, "machineGun");
 	}
 }
--- sentry/src/main/java/icbm/sentry/items/weapons/conventional/ItemShotgun.java
+++ sentry/src/main/java/icbm/sentry/items/weapons/conventional/ItemShotgun.java
@@ -3,7 +3,7 @@ package icbm.sentry.items.weapons.conventional;
 public class ItemShotgun extends ItemConventional {
 
 	public ItemShotgun(int id) {
-		super(id, 5, 2, 5, "shotgun");
+		super(id, 5, "shotgun");
 	}
 
 }
