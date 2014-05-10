9fd483c5a1bdf595d2094935ccb85de649f01a08
Archtikz <atroglyphics@gmail.com>
2014-05-10 12:40:55 +0100

    Forgot to commit this.

--- sentry/src/main/java/icbm/sentry/items/weapons/conventional/ItemAssaultRifle.java
+++
@@ -1,9 +0,0 @@
-package icbm.sentry.items.weapons.conventional;
-
-public class ItemAssaultRifle extends ItemConventional {
-
-	public ItemAssaultRifle(int id) {
-		super(id, "assaultRifle", 24, 5, "assaultRifle");
-	}
-
-}
