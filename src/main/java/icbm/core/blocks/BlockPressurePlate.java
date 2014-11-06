2ce5c56397e0d1c2b1d37a0ee181774bfb63f488
Robert S <rseifert.phone@gmail.com>
2014-11-05 22:47:12 -0500

    Temp removed sentry module, reworked glass plate & button, worked on Detector GUI

---
+++ src/main/java/icbm/core/blocks/BlockPressurePlate.java
@@ -0,0 +1,14 @@
+package icbm.core.blocks;
+
+import net.minecraft.block.material.Material;
+
+/**
+ * Created by robert on 11/5/2014.
+ */
+public class BlockPressurePlate extends net.minecraft.block.BlockPressurePlate
+{
+    public BlockPressurePlate(String p_i45418_1_, Material p_i45418_2_, Sensitivity p_i45418_3_)
+    {
+        super(p_i45418_1_, p_i45418_2_, p_i45418_3_);
+    }
+}
