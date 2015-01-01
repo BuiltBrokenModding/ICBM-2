be09c82144723b74807a77b7aebc431ec9e276f6
Robert S <rseifert.phone@gmail.com>
2015-01-01 03:08:09 -0500

    Updated MavenPageMaker

---
+++ bin-maven/html/downloads/downloads-1.7.10.php
@@ -0,0 +1,249 @@
+<!--Generated using Maven Download Page Maker by Robert Seifert-->
+<!--Project github https://github.com/DarkGuardsman/MavenPageMaker -->
+<!--Page Created on Thu Jan 01 03:07:08 EST 2015-->
+<div id="maven-build-div-1.7.10" class="maven-downloads">
+<h3> Minecraft 1.7.10 Downloads </h3>
+	<table id="maven-build-table-1.7.10">
+<thead><tr><td>Version</td><td>Files</td></tr></thead>      
+	<tr>
+		<td><span>2.0.0</span></br>#4</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#4</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#5</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#6</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#9</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#10</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#11</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#12</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#13</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#14</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#15</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#16</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#17</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#18</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#19</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#20</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#21</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#22</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#23</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#24</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#25</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#26</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#27</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.0.0</span></br>#31</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.2.0</span></br>#32</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.2.0</span></br>#33</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.2.0</span></br>#34</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.2.0</span></br>#35</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.2.0</span></br>#36</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>      
+	<tr>
+		<td><span>2.2.0</span></br>#37</td><td>
+			<ul>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBM.jar</a></li>
+				<li><a href="http://ci.builtbroken.com/maven/icbm/ICBM/http://adf.ly/2380428/" target="_blank">ICBMdeobf.jar</a></li>
+			</ul>
+		</td>
+	</tr>
+</table>
+</div>
