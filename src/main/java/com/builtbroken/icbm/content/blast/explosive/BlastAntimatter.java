1fe482b2b0ce52664fbe0e6b5cff56e64eeaba09
Robert S <rseifert.phone@gmail.com>
2015-03-26 01:04:48 -0400

    Notes

--- src/main/java/com/builtbroken/icbm/content/blast/explosive/BlastAntimatter.java
+++ src/main/java/com/builtbroken/icbm/content/blast/explosive/BlastAntimatter.java
@@ -7,14 +7,20 @@ import com.builtbroken.mc.prefab.explosive.blast.BlastSimplePath;
 import net.minecraft.init.Blocks;
 
 /**
+ * Blast that will destroy everything ignoring resistance to explosives
+ * as Antimatter + matter(everything) = nothing(energy but no objects). In
+ * other words it doesn't actually destory the blocks using a normal blast. Instead
+ * it simple null out the block at the location.
  * Created by robert on 3/25/2015.
  */
 public class BlastAntimatter extends BlastSimplePath
 {
+    //TODO add delay secondary blast trigger that uses energy released to destroy more blocks
+
     @Override
     public BlockEdit changeBlock(Location location)
     {
-        if(location.getBlock() == Blocks.air)
+        if (location.getBlock() == Blocks.air)
             return null;
         return new BlockEdit(location).set(Blocks.air, 0, false, false);
     }
@@ -22,7 +28,7 @@ public class BlastAntimatter extends BlastSimplePath
     @Override
     public boolean shouldPath(Location location)
     {
-        if(!ICBM.ANTIMATTER_BREAK_UNBREAKABLE && location.getHardness() < 0)
+        if (!ICBM.ANTIMATTER_BREAK_UNBREAKABLE && location.getHardness() < 0)
             return false;
         return super.shouldPath(location);
     }
