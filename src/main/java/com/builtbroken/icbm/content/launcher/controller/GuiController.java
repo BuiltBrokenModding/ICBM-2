a0a7ff7147fd00ca8c16cf5bc87da6490f3994fd
Robert S <rseifert.phone@gmail.com>
2015-04-21 18:31:31 -0400

    Coded a little functionality into controller GUI

--- src/main/java/com/builtbroken/icbm/content/launcher/controller/GuiController.java
+++ src/main/java/com/builtbroken/icbm/content/launcher/controller/GuiController.java
@@ -1,12 +1,14 @@
 package com.builtbroken.icbm.content.launcher.controller;
 
+import com.builtbroken.icbm.content.launcher.TileAbstractLauncher;
 import com.builtbroken.mc.core.References;
 import com.builtbroken.mc.lib.helper.LanguageUtility;
 import com.builtbroken.mc.prefab.gui.ContainerDummy;
 import com.builtbroken.mc.prefab.gui.GuiContainerBase;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.entity.player.EntityPlayer;
-import org.lwjgl.input.Keyboard;
+import net.minecraft.tileentity.TileEntity;
 
 /**
  * Created by robert on 2/20/2015.
@@ -17,7 +19,13 @@ public class GuiController extends GuiContainerBase
 
     GuiButton[] buttons;
 
-    int page = 0;
+    protected GuiTextField x_field;
+    protected GuiTextField y_field;
+    protected GuiTextField z_field;
+    protected String errorString = "";
+
+    boolean editMode = false;
+    int editMissile = 0;
 
 
     public GuiController(TileController launcher, EntityPlayer player)
@@ -34,16 +42,43 @@ public class GuiController extends GuiContainerBase
 
         int x = this.guiLeft + 20;
         int y = this.guiTop + 20;
-        if(controller.launcherData != null)
+
+        if (!editMode)
         {
-            for(int i = 0; i < controller.launcherData.size(); i++)
+            if (controller.launcherData != null)
             {
-                buttons = new GuiButton[controller.launcherData.size() * 2];
-                buttons[i] = new GuiButton(i, x, y, 100, 20, "Launcher " + i);
-                this.buttonList.add(buttons[i]);
-                buttons[i + controller.launcherData.size()] = new GuiButton(i + controller.launcherData.size(), x + 105, y, 30, 20, "Fire");
-                this.buttonList.add(buttons[i + controller.launcherData.size()]);
-                y += 22;
+                for (int i = 0; i < controller.launcherData.size(); i++)
+                {
+                    buttons = new GuiButton[controller.launcherData.size() * 2];
+
+                    //Launcher edit button
+                    buttons[i] = new GuiButton(i, x, y, 100, 20, "Launcher " + i);
+                    this.buttonList.add(buttons[i]);
+
+                    //Fire launcher button
+                    buttons[i + controller.launcherData.size()] = new GuiButton(i + controller.launcherData.size(), x + 105, y, 30, 20, "Fire");
+                    if (controller.launcherData.get(i).missile == null)
+                        buttons[i + controller.launcherData.size()].enabled = false;
+                    this.buttonList.add(buttons[i + controller.launcherData.size()]);
+                    y += 22;
+                }
+            }
+        }
+        else
+        {
+            this.buttonList.add(new GuiButton(0, x, y, 30, 20, "Back"));
+            this.buttonList.add(new GuiButton(1, x + 100, y + 100, 30, 20, "Update"));
+
+            x = guiLeft + 10;
+            y = guiTop + 40;
+
+            TileEntity tile = controller.launcherData.get(editMissile).location.getTileEntity();
+
+            if(tile instanceof TileAbstractLauncher && ((TileAbstractLauncher) tile).target != null)
+            {
+                this.x_field = this.newField(x, y, 30, 20, "" + ((TileAbstractLauncher) tile).target.xi());
+                this.y_field = this.newField(x + 35, y, 30, 20, "" + ((TileAbstractLauncher) tile).target.yi());
+                this.z_field = this.newField(x + 75, y, 30, 20, "" + ((TileAbstractLauncher) tile).target.zi());
             }
         }
     }
@@ -52,6 +87,33 @@ public class GuiController extends GuiContainerBase
     protected void actionPerformed(GuiButton button)
     {
         super.actionPerformed(button);
+        if (!editMode)
+        {
+            if (button.id >= 0 && button.id < buttons.length)
+            {
+                //Edit launcher button
+                if (button.id < controller.launcherData.size())
+                {
+                    editMissile = button.id;
+                    editMode = true;
+                    initGui();
+                }
+                //Fire launcher
+                else
+                {
+                    controller.fireLauncher(button.id - controller.launcherData.size());
+                }
+            }
+        }
+        else
+        {
+            if(button.id == 0)
+            {
+                editMissile = -1;
+                editMode = false;
+                initGui();
+            }
+        }
     }
 
     @Override
@@ -60,7 +122,10 @@ public class GuiController extends GuiContainerBase
         super.drawGuiContainerForegroundLayer(mouseX, mouseY);
         drawStringCentered(LanguageUtility.getLocalName(controller.getInventoryName()), 85, 10);
 
-
+        if (!editMode && controller.launcherData != null && controller.launcherData.size() == 0)
+        {
+            drawStringCentered("No linked silos", 85, 40);
+        }
     }
 
     @Override
