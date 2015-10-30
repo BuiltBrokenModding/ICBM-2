1b0ab1985cf146ee5739705a7751a6a537e682ca
Robert Seifert <rseifert.phone@gmail.com>
2015-10-30 14:30:43 -0400

    Fixed endo and exo thermal blasts looping causing stack overflow errors

--- src/main/java/com/builtbroken/icbm/content/blast/BlastEndoThermic.java
+++ src/main/java/com/builtbroken/icbm/content/blast/BlastEndoThermic.java
@@ -28,7 +28,7 @@ public class BlastEndoThermic extends BlastSimplePath
         //TODO change temp to be based on init energy and heating data of the block
         //TODO change to dump heat into the heat map
         PlacementData data = HeatedBlockRegistry.getResultCoolDown(block, getTempForDistance(location.distance(x, y, z)));
-        if(data != null && data.block() != null)
+        if (data != null && data.block() != null)
         {
             BlockEdit edit = new BlockEdit(location);
             edit.set(data.block(), data.meta() == -1 ? 0 : data.meta(), false, true);
@@ -37,7 +37,7 @@ public class BlastEndoThermic extends BlastSimplePath
         else if (location.isAirBlock())
         {
             Location loc = location.add(0, -1, 0);
-            if(!loc.isAirBlock() && loc.isSideSolid(ForgeDirection.UP))
+            if (!loc.isAirBlock() && loc.isSideSolid(ForgeDirection.UP))
             {
                 BlockEdit edit = new BlockEdit(location);
                 edit.set(Blocks.snow_layer, 0, false, true);
@@ -47,15 +47,31 @@ public class BlastEndoThermic extends BlastSimplePath
         return null;
     }
 
+    @Override
+    public boolean shouldPathTo(Location last, Location next)
+    {
+        if (super.shouldPathTo(last, next))
+        {
+            if (last.isAirBlock() && next.isAirBlock())
+                return last.sub(next).toForgeDirection() != ForgeDirection.UP;
+            PlacementData data = HeatedBlockRegistry.getResultWarmUp(next.getBlock(), getTempForDistance(next.distance(x, y, z)));
+            PlacementData dataLast = HeatedBlockRegistry.getResultWarmUp(last.getBlock(), getTempForDistance(last.distance(x, y, z)));
+            if ((data == null || data.block() == null) && (dataLast == null || dataLast.block() == null))
+                return false;
+            return true;
+        }
+        return false;
+    }
+
     private int getTempForDistance(double distance)
     {
-        return 0 + (int)(Math.max(10, (293 / size)) * distance);
+        return 0 + (int) (Math.max(10, (293 / size)) * distance);
     }
 
     @Override
     public void doEffectOther(boolean beforeBlocksPlaced)
     {
-        if(!beforeBlocksPlaced)
+        if (!beforeBlocksPlaced)
         {
             List<Entity> list = EntitySelectors.LIVING_SELECTOR.selector().getEntities(this, size * 2);
             for (Entity entity : list)
--- src/main/java/com/builtbroken/icbm/content/blast/BlastExoThermic.java
+++ src/main/java/com/builtbroken/icbm/content/blast/BlastExoThermic.java
@@ -3,7 +3,6 @@ package com.builtbroken.icbm.content.blast;
 import com.builtbroken.mc.lib.transform.vector.Location;
 import com.builtbroken.mc.lib.world.edit.BlockEdit;
 import com.builtbroken.mc.lib.world.edit.PlacementData;
-import com.builtbroken.mc.lib.world.heat.BlockConversionData;
 import com.builtbroken.mc.lib.world.heat.HeatedBlockRegistry;
 import com.builtbroken.mc.prefab.entity.selector.EntitySelectors;
 import com.builtbroken.mc.prefab.explosive.blast.BlastSimplePath;
@@ -21,11 +20,12 @@ import java.util.List;
 public class BlastExoThermic extends BlastSimplePath
 {
     public static DamageSource thermalDamage = new DamageSource("thermal").setFireDamage();
+
     @Override
     public BlockEdit changeBlock(Location location)
     {
         Block block = location.getBlock();
-        if(block != null)
+        if (block != null)
         {
             //TODO change temp to be based on init energy and heating data of the block
             //TODO change to dump heat into the heat map
@@ -39,7 +39,7 @@ public class BlastExoThermic extends BlastSimplePath
             else if (location.isAirBlock())
             {
                 Location loc = location.add(0, -1, 0);
-                if(!loc.isAirBlock() && loc.isSideSolid(ForgeDirection.UP))
+                if (!loc.isAirBlock() && loc.isSideSolid(ForgeDirection.UP))
                 {
                     BlockEdit edit = new BlockEdit(location);
                     edit.set(Blocks.fire, 0, false, true);
@@ -50,15 +50,31 @@ public class BlastExoThermic extends BlastSimplePath
         return null;
     }
 
+    @Override
+    public boolean shouldPathTo(Location last, Location next)
+    {
+        if (super.shouldPathTo(last, next))
+        {
+            if (last.isAirBlock() && next.isAirBlock())
+                return last.sub(next).toForgeDirection() != ForgeDirection.UP;
+            PlacementData data = HeatedBlockRegistry.getResultWarmUp(next.getBlock(), getTempForDistance(next.distance(x, y, z)));
+            PlacementData dataLast = HeatedBlockRegistry.getResultWarmUp(last.getBlock(), getTempForDistance(last.distance(x, y, z)));
+            if ((data == null || data.block() == null) && (dataLast == null || dataLast.block() == null))
+                return false;
+            return true;
+        }
+        return false;
+    }
+
     private int getTempForDistance(double distance)
     {
-        return 20000 - (int)((20000 / size) * distance);
+        return 20000 - (int) ((20000 / size) * distance);
     }
 
     @Override
     public void doEffectOther(boolean beforeBlocksPlaced)
     {
-        if(!beforeBlocksPlaced)
+        if (!beforeBlocksPlaced)
         {
             List<Entity> list = EntitySelectors.LIVING_SELECTOR.selector().getEntities(this, size * 2);
             for (Entity entity : list)
