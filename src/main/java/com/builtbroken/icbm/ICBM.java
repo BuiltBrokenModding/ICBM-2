6d9c5e5598a11df3bd5d314b72b949369fd7e991
Robert Seifert <rseifert.phone@gmail.com>
2015-10-30 07:16:45 -0400

    Added request for circuits to load from VE, Updated to VE version 0.6.6

--- src/main/java/com/builtbroken/icbm/ICBM.java
+++ src/main/java/com/builtbroken/icbm/ICBM.java
@@ -161,6 +161,7 @@ public final class ICBM extends AbstractMod
         Engine.requestSheetMetalContent();
         Engine.requestMultiBlock();
         Engine.requestSimpleTools();
+        Engine.requestCircuits();
 
         // Configs TODO load up using config system, and separate file
         ANTIMATTER_BREAK_UNBREAKABLE = getConfig().getBoolean("Antimatter_Destroy_Unbreakable", Configuration.CATEGORY_GENERAL, true, "Allows antimatter to break blocks that are unbreakable, bedrock for example.");
