ca39c5040ae4bf2b2373f630c6da01a69562da8e
Robert Seifert <rseifert.phone@gmail.com>
2015-12-04 03:07:08 -0500

    Got ISBR working though I think there will be a bug later, blame z

--- src/main/java/com/builtbroken/icbm/content/launcher/block/ISBRLauncherFrame.java
+++ src/main/java/com/builtbroken/icbm/content/launcher/block/ISBRLauncherFrame.java
@@ -8,9 +8,7 @@ import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.init.Blocks;
 import net.minecraft.world.IBlockAccess;
-import net.minecraftforge.client.model.obj.WavefrontObject;
 import org.lwjgl.opengl.GL11;
 
 /**
@@ -42,14 +40,25 @@ public class ISBRLauncherFrame implements ISimpleBlockRenderingHandler
     @Override
     public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer)
     {
-        GL11.glPushMatrix();
+        //GL11.glPushMatrix();
         //GL11.glTranslatef(x, y, z);
         //GL11.glScalef(1.5f, 1.5f, 1.5f);
+        //FMLClientHandler.instance().getClient().renderEngine.bindTexture(Assets.GREY_FAKE_TEXTURE);
+        //((WavefrontObject) Assets.LAUNCHER_FRAME_BLOCK_MODEL).tessellateAll(Tessellator.instance);
+        //GL11.glPopMatrix();
+        //FMLClientHandler.instance().getClient().renderEngine.bindTexture(TextureMap.locationBlocksTexture);
+        //renderer.renderBlockAllFaces(Blocks.fence, x, y, z);
+
+        Tessellator tess = Tessellator.instance;
+        tess.draw();
+        GL11.glPushMatrix();
+        GL11.glTranslatef(x, y, z + 1);
+        GL11.glColor3f(1, 1, 1);
         FMLClientHandler.instance().getClient().renderEngine.bindTexture(Assets.GREY_FAKE_TEXTURE);
-        ((WavefrontObject) Assets.LAUNCHER_FRAME_BLOCK_MODEL).tessellateAll(Tessellator.instance);
+        Assets.LAUNCHER_FRAME_BLOCK_MODEL.renderAll();
         GL11.glPopMatrix();
         FMLClientHandler.instance().getClient().renderEngine.bindTexture(TextureMap.locationBlocksTexture);
-        renderer.renderBlockAllFaces(Blocks.fence, x, y, z);
+        tess.startDrawingQuads();
         return true;
     }
 
