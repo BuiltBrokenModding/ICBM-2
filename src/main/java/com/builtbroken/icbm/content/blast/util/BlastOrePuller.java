0c2411dcba91387e85acff766511e0c61c30b552
Robert Seifert <rseifert.phone@gmail.com>
2017-08-29 17:29:20 -0400

    Rewrote ore puller explosive to work in a cube area ignoreing y level

--- src/main/java/com/builtbroken/icbm/content/blast/util/BlastOrePuller.java
+++ src/main/java/com/builtbroken/icbm/content/blast/util/BlastOrePuller.java
@@ -1,18 +1,16 @@
 package com.builtbroken.icbm.content.blast.util;
 
+import com.builtbroken.mc.api.edit.IWorldChangeLayeredAction;
 import com.builtbroken.mc.api.edit.IWorldEdit;
 import com.builtbroken.mc.api.explosive.IExplosiveHandler;
-import com.builtbroken.mc.core.content.resources.ore.BlockOre;
 import com.builtbroken.mc.core.content.resources.gems.BlockGemOre;
+import com.builtbroken.mc.core.content.resources.ore.BlockOre;
+import com.builtbroken.mc.framework.explosive.blast.Blast;
 import com.builtbroken.mc.imp.transform.vector.BlockPos;
 import com.builtbroken.mc.imp.transform.vector.Location;
-import com.builtbroken.mc.imp.transform.vector.Pos;
 import com.builtbroken.mc.lib.world.edit.BlockEdit;
 import com.builtbroken.mc.lib.world.edit.BlockEditMove;
-import com.builtbroken.mc.prefab.explosive.blast.BlastSimplePath;
 import net.minecraft.block.Block;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.FurnaceRecipes;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -24,10 +22,14 @@ import java.util.List;
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 4/28/2016.
  */
-public class BlastOrePuller extends BlastSimplePath<BlastOrePuller>
+public class BlastOrePuller extends Blast<BlastOrePuller> implements IWorldChangeLayeredAction
 {
     public static final List<Block> whiteList = new ArrayList();
     public static final List<Block> blackList = new ArrayList();
+
+    private int layers;
+    private int sideLength;
+    private int sizeInt;
 
     public BlastOrePuller(IExplosiveHandler handler)
     {
@@ -35,14 +37,53 @@ public class BlastOrePuller extends BlastSimplePath<BlastOrePuller>
     }
 
     @Override
-    public void getEffectedBlocks(List<IWorldEdit> list)
+    public BlastOrePuller setYield(double size)
     {
-        super.getEffectedBlocks(list);
+        double prev = this.size;
+        super.setYield(Math.floor(size));
+        if (prev != size)
+        {
+            calculateLayers();
+        }
+        return this;
+    }
 
+    public void calculateLayers()
+    {
+        sizeInt = (int) Math.floor(this.size);
+        sideLength = sizeInt * 2 + 1;
+        layers = sideLength * sideLength;
+    }
+
+
+    @Override
+    public void getEffectedBlocks(List<IWorldEdit> list, int layer)
+    {
+        int gridX = layer % sideLength;
+        int gridZ = layer / sideLength;
+
+        int x = gridX + xi() - sizeInt;
+        int z = gridZ + zi() - sizeInt;
+
+        for (int y = 0; y < yi(); y++)
+        {
+            BlockPos pos = new BlockPos(x, y, z);
+            IWorldEdit edit = changeBlock(pos);
+            if (edit != null)
+            {
+                list.add(edit);
+            }
+        }
+
+        createMovePositions(list);
+    }
+
+    protected void createMovePositions(List<IWorldEdit> list)
+    {
         //Sort threw list and find them new homes
         Iterator<IWorldEdit> it = list.iterator();
         List<IWorldEdit> newList = new ArrayList();
-        List<Pos> usedSurfacePos = new ArrayList();
+        List<BlockPos> usedSurfacePos = new ArrayList();
 
         //Loop over all edits we have already mapped
         while (it.hasNext())
@@ -53,12 +94,12 @@ public class BlastOrePuller extends BlastSimplePath<BlastOrePuller>
                 it.remove();
                 continue;
             }
-            int y = 0;
+            int y = edit.yi(); //Start at edit location to prevent pulling ore down
 
             //Loop up to find a valid location to place the blocks
             while (y++ < 255)
             {
-                Pos pos = new Pos((int) edit.x(), y, (int) edit.z());
+                BlockPos pos = new BlockPos(edit.xi(), y, edit.zi());
 
                 //Ensure the slot is not already used
                 if (!usedSurfacePos.contains(pos))
@@ -78,18 +119,17 @@ public class BlastOrePuller extends BlastSimplePath<BlastOrePuller>
                 it.remove();
                 continue;
             }
-            //Add placement call TODO make new block edit class called BlockMove to ensure the original location is also still valid and to reverse if the final location is not valid
+            //Add placement call
             newList.add(new BlockEditMove(edit, new Location(edit.oldWorld(), edit.x(), y, edit.z())).setAir());
-            it.remove(); //Always remove as we now uses the move edit
+            it.remove();
         }
         list.addAll(newList);
     }
 
-    @Override
-    public IWorldEdit changeBlock(BlockPos location)
+    protected IWorldEdit changeBlock(BlockPos pos)
     {
-        Block block = location.getBlock(oldWorld);
-        if (!location.canSeeSky(oldWorld))
+        Block block = pos.getBlock(oldWorld);
+        if (!pos.canSeeSky(oldWorld))
         {
             if (blackList.contains(block))
             {
@@ -97,28 +137,21 @@ public class BlastOrePuller extends BlastSimplePath<BlastOrePuller>
             }
             else if (block instanceof BlockOre || block instanceof net.minecraft.block.BlockOre || block instanceof BlockGemOre || whiteList.contains(block))
             {
-                return new BlockEdit(oldWorld, location).setAir();
-            }
-            else if (block.getUnlocalizedName().contains("ore"))
-            {
-                List<ItemStack> stacks = block.getDrops(oldWorld, location.xi(), location.yi(), location.zi(), 0, 0);
-                if (stacks != null)
-                {
-                    for (ItemStack stack : stacks)
-                    {
-                        ItemStack result = FurnaceRecipes.smelting().getSmeltingResult(stack);
-                        if (result != null && result.getUnlocalizedName().contains("ingot")) // If it can smelt it is ore
-                        {
-                            //Add to white list to increase speed
-                            whiteList.add(block);
-                            return new BlockEdit(oldWorld, location).setAir();
-                        }
-                    }
-                }
-                //Auto add to black list to increase speed
-                blackList.add(block);
+                return new BlockEdit(oldWorld, pos).setAir();
             }
         }
         return null;
+    }
+
+    @Override
+    public int getLayers()
+    {
+        return layers;
+    }
+
+    @Override
+    public boolean shouldContinueAction(int layer)
+    {
+        return !killExplosion;
     }
 }
